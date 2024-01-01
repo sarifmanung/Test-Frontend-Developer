e300fe81b3607a38615f0bf16be8229f9882c15b
sarifmanung <406168026@yru.ac.th>
2024-01-01 19:00:15 +0700

    testingform

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 import Homepage from "./Pages/Homepage";
 import Testlang from "./Pages/Testing/Testlang";
+import FormTest from "./Pages/Testing/FormTest.tsx";
 import Test1 from "./Pages/Test1";
 import Test2 from "./Pages/Test2";
 
@@ -15,6 +16,7 @@ function App() {
           <Route path="/test1" element={<Test1 />} />{" "}
           <Route path="/test2" element={<Test2 />} />{" "}
           <Route path="/testlang" element={<Testlang />} />{" "}
+          <Route path="/formtest" element={<FormTest />} />{" "}
         </Routes>{" "}
       </div>{" "}
     </BrowserRouter>
