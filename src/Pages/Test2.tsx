274d0cf57130fd5ffa8f7e6b663c77298b65afa2
sarifmanung <406168026@yru.ac.th>
2024-01-01 21:12:00 +0700

    updated

--- src/Pages/Test2.tsx
+++ src/Pages/Test2.tsx
@@ -3,6 +3,7 @@ import { Form, Input, Button, Select, Row, Col, Radio, DatePicker } from "antd";
 import { useDispatch } from "react-redux";
 import { addFormData } from "../redux/formDataSlice";
 import TableComponent from "../components/TableComponent";
+import "./Test2.scss";
 
 const { Option } = Select;
 
@@ -28,112 +29,123 @@ const Test2 = () => {
   };
 
   return (
-    <div style={{ maxWidth: "900px", margin: "auto" }}>
-      <h1> Ant Design Form </h1>{" "}
-      <Form form={form} onFinish={onFinish} layout="vertical">
-        <Row gutter={16}>
-          <Col span={4}>
-            <Form.Item
-              name="prefix"
-              label="Prefix"
-              rules={[{ required: true }]}
-            >
-              <Select
-                placeholder="Select a option and change input text above"
-                allowClear
-              >
-                <Option value="Mr.">Mr</Option>
-                <Option value="Mrs.">Mrs</Option>
-                <Option value="Miss.">Miss</Option>
-              </Select>
-            </Form.Item>
-          </Col>{" "}
-          <Col span={10}>
-            <Form.Item
-              label="First Name"
-              name="firstName"
-              rules={[
-                { required: true, message: "Please enter your first name" },
-              ]}
-            >
-              <Input />
-            </Form.Item>{" "}
-          </Col>{" "}
-          <Col span={10}>
-            <Form.Item
-              label="Last Name"
-              name="lastName"
-              rules={[
-                { required: true, message: "Please enter your last name" },
-              ]}
-            >
-              <Input />
-            </Form.Item>{" "}
-          </Col>{" "}
-        </Row>{" "}
-        <Row gutter={16}>
-          <Col span={10}>
-            <Form.Item
-              label="Date of Birth"
-              name="dateOfBirth"
-              rules={[{ required: true }]}
-            >
-              <DatePicker style={{ width: "100%" }} />
-            </Form.Item>
-          </Col>{" "}
-          <Col span={10}>
-            <Form.Item
-              name="nationality"
-              label="สัญชาติ"
-              rules={[{ required: true }]}
-            >
-              <Select placeholder="--กรุณาเลือก--" allowClear>
-                <Option value="Thai nationality">สัญชาติไทย</Option>
-              </Select>
-            </Form.Item>
-          </Col>{" "}
-        </Row>{" "}
-        <Row gutter={16}>
-          <Col span={20}>
-            <Form.Item
-              label="Gender"
-              name="gender"
-              rules={[{ required: true }]}
-            >
-              <Radio.Group>
-                <Radio value="male">male</Radio>
-                <Radio value="female">female</Radio>
-                <Radio value="other">other</Radio>
-              </Radio.Group>
-            </Form.Item>
-          </Col>{" "}
-        </Row>{" "}
-        <Row gutter={16}>
-          <Col span={20}>
+    <>
+      <div className="section1">
+        <div
+          className="form_container"
+          style={{ maxWidth: "900px", margin: "auto" }}
+        >
+          <h2> จัดการหน้าฟอร์ม </h2>{" "}
+          <Form
+            form={form}
+            onFinish={onFinish}
+            className="form_data"
+            layout="vertical"
+          >
+            <Row gutter={16}>
+              <Col span={4}>
+                <Form.Item
+                  name="prefix"
+                  label="Prefix"
+                  rules={[{ required: true }]}
+                >
+                  <Select
+                    placeholder="Select a option and change input text above"
+                    allowClear
+                  >
+                    <Option value="Mr.">Mr</Option>
+                    <Option value="Mrs.">Mrs</Option>
+                    <Option value="Miss.">Miss</Option>
+                  </Select>
+                </Form.Item>
+              </Col>{" "}
+              <Col span={10}>
+                <Form.Item
+                  label="First Name"
+                  name="firstName"
+                  rules={[
+                    { required: true, message: "Please enter your first name" },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>{" "}
+              </Col>{" "}
+              <Col span={10}>
+                <Form.Item
+                  label="Last Name"
+                  name="lastName"
+                  rules={[
+                    { required: true, message: "Please enter your last name" },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>{" "}
+              </Col>{" "}
+            </Row>{" "}
+            <Row gutter={16}>
+              <Col span={10}>
+                <Form.Item
+                  label="Date of Birth"
+                  name="dateOfBirth"
+                  rules={[{ required: true }]}
+                >
+                  <DatePicker style={{ width: "100%" }} />
+                </Form.Item>
+              </Col>{" "}
+              <Col span={10}>
+                <Form.Item
+                  name="nationality"
+                  label="สัญชาติ"
+                  rules={[{ required: true }]}
+                >
+                  <Select placeholder="--กรุณาเลือก--" allowClear>
+                    <Option value="Thai nationality">สัญชาติไทย</Option>
+                  </Select>
+                </Form.Item>
+              </Col>{" "}
+            </Row>{" "}
+            <Row gutter={16}>
+              <Col span={20}>
+                <Form.Item
+                  label="Gender"
+                  name="gender"
+                  rules={[{ required: true }]}
+                >
+                  <Radio.Group>
+                    <Radio value="male">male</Radio>
+                    <Radio value="female">female</Radio>
+                    <Radio value="other">other</Radio>
+                  </Radio.Group>
+                </Form.Item>
+              </Col>{" "}
+            </Row>{" "}
             <Form.Item label="ID Card Number" name="idCardNumber">
               <Input />
             </Form.Item>
-          </Col>
-        </Row>
-        <Form.Item
-          name="phone"
-          label="Phone Number"
-          rules={[
-            { required: true, message: "Please input your phone number!" },
-          ]}
-        >
-          <Input addonBefore={phone_prefixSelector} style={{ width: "100%" }} />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit{" "}
-          </Button>{" "}
-        </Form.Item>{" "}
-      </Form>{" "}
-      <div>
-        <h2> Table </h2> <TableComponent /> {/* Use the TableComponent here */}{" "}
-      </div>{" "}
-    </div>
+            <Form.Item
+              name="phone"
+              label="Phone Number"
+              rules={[
+                { required: true, message: "Please input your phone number!" },
+              ]}
+            >
+              <Input
+                addonBefore={phone_prefixSelector}
+                style={{ width: "100%" }}
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Submit{" "}
+              </Button>{" "}
+            </Form.Item>{" "}
+          </Form>{" "}
+        </div>
+        <div style={{ maxWidth: "1100px", margin: "auto" }}>
+          <TableComponent />
+        </div>{" "}
+      </div>
+    </>
   );
 };
 
